881b34292cbe8013d16d99dc6e8fb8704e43d3b9
AasifSaneeth <alasaneeth22@gmail.com>
2025-04-19 02:32:56 -0400

    Feat:Server restart with nodemon

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -3,18 +3,46 @@ const User = require('../models/userModel');
 require('dotenv').config();
 
 const studentRegister = async (req, res) => {
-    const {fullName, gender,dob,phone,email,address,status,enrolmentDate,widthrowelDate, username, password ,userRole} = req.body;
-    console.log('Request Body:', req.body);
-    try {
-      const sutudent = await Student.create(fullName, gender,dob,phone,email,address,status,enrolmentDate,widthrowelDate); 
-      const userId = sutudent.id;
+  const {
+    fullName,
+    gender,
+    dob,
+    phone,
+    email,
+    address,
+    status,
+    enrolmentDate,
+    widthrowelDate,
+    username,
+    password,
+    userRole
+  } = req.body;
 
-      const user = await User.create();
-      //const userId = await User.create(username, password ,userRole);
-      res.status(201).json({ message: 'User created successfully', sutudent});
-    } catch (error) {
-      res.status(500).json({ message: 'Error creating user', error });
-    }
-  };
+  //console.log('Request Body:', req.body);
 
-  module.exports = {studentRegister};
+  try {
+    const student = await Student.create({
+      fullName,
+      gender,
+      dob,
+      phone,
+      email,
+      address,
+      status,
+      enrolmentDate,
+      widthrowelDate
+    });
+
+    const userId = student.insertId;
+
+    res.status(201).json({"userId": userId });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error creating user', error });
+  }
+};
+
+
+
+
+module.exports = { studentRegister };
--- models/studentModel.js
+++ models/studentModel.js
@@ -2,11 +2,12 @@ const db = require('../config/db');
 
 const Student = {
     
-  create: async (fullName, gender,dob,phone,email,address,status,enrolmentDate,widthrowelDate) => {
+  create: async ({ fullName, gender, dob, phone, email, address, status, enrolmentDate, widthrowelDate }) => {
 
     const [result] = await db.execute('INSERT INTO students (fullName, gender,dob,phone,email,address,status,enrolmentDate,widthrowelDate) VALUES (?,?,?,?,?,?,?,?,?)', 
                     [fullName, gender,dob,phone,email,address,status,enrolmentDate,widthrowelDate]);
-                    return result.insertId;
+                    
+                    return result;
   },
 };
 
